08b9c6abfe0c91b157b5df26ece696acf3e25ac7
Antoschkka32 <100727940+Antoschkka32@users.noreply.github.com>
2022-03-25 16:28:50 +0300

    Create antondrobi.h

---
+++ antondrobi.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int count;
+int chisl1, znam1, chisl2, znam2, a;
+int  chislob, znamob, x;
+
+void vvod(int& cisl1, int& znam1, int& cisl2, int& znam2);
+
+void vivod(int chislob, int znamob);
+
+int sokr(int a, int b);
